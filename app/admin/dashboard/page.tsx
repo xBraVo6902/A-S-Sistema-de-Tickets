6a1361ba843ab47865f220e08589c9e1dbbe08ee
nachitopopwine <113112079+nachitopopwine@users.noreply.github.com>
2024-12-10 23:42:22 -0300

    a

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Suspense, useEffect, useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
