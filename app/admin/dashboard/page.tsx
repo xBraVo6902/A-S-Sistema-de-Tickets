2cd340c9be3fad837ae1dd523b35f19c6d2687da
nachitopopwine <113112079+nachitopopwine@users.noreply.github.com>
2024-12-11 12:42:32 -0300

    Revert "a"
    This reverts commit 6a1361ba843ab47865f220e08589c9e1dbbe08ee.

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Suspense, useEffect, useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
