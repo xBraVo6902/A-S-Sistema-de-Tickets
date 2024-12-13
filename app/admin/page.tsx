b6f1773ad3f3d734142886c27c26423b7f25f615
MaxSteel <maxm2004.mc@gmail.com>
2024-12-12 22:57:01 -0300

    Admin - gestion usuarios

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-//import Image from "next/image";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import {
