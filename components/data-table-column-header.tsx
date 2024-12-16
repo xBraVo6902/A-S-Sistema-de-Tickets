89e0c604706e53aacbf360f3fb0652c16c1303b4
Pablo <pablocortes1101@gmail.com>
2024-12-15 21:35:39 -0300

    feat: remover opcion para ocultar columna

--- components/data-table-column-header.tsx
+++ components/data-table-column-header.tsx
@@ -54,11 +54,6 @@ export function DataTableColumnHeader<TData, TValue>({
             <ArrowDown className="h-3.5 w-3.5 text-muted-foreground/70" />
             Desc
           </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => column.toggleVisibility(false)}>
-            <EyeOff className="h-3.5 w-3.5 text-muted-foreground/70" />
-            Hide
-          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
