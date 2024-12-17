48a1e30abdcdd95ea18688928d3dafdb7ffc0ea0
Pablo <pablocortes1101@gmail.com>
2024-12-17 20:42:38 -0300

    feat: agregar validacion en create-ticket-form

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,8 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import prisma from "@/lib/db";
-import { Status } from "@prisma/client";
-import { ticketMetadata } from "@/prisma/ticketMetadata";
+import { Status, Ticket } from "@prisma/client";
 
 export async function assignUserToTicket(ticketId: string, userId: string) {
   try {
@@ -33,4 +32,11 @@ export async function updateTicketStatus(ticketId: string, status: Status) {
     console.error("Failed to update status:", error);
     return { success: false };
   }
+}
+
+export async function searchClientByRut(rut: string) {
+  const client = await prisma.person.findUnique({
+    where: { rut: rut },
+  });
+  return client;
 }
