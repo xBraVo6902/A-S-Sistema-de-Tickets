edb03e5375cb2a549a5faf8511406df5c1608bd3
Pablo <pablocortes1101@gmail.com>
2024-12-19 02:11:10 -0300

    fix: eliminar link de recuperacion de contraseña

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,4 @@
 import LoginForm from "@/components/login-form";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default function LoginPage() {
@@ -8,12 +7,6 @@ export default function LoginPage() {
       <Suspense>
         <div className="">
           <LoginForm />
-          <p className="text-sm text-gray-600 mt-4">
-            ¿Has olvidado tu contraseña?{" "}
-            <Link href="/forgot-password" className="text-blue-600 hover:underline">
-              Recupérala aquí
-            </Link>
-          </p>
         </div>
       </Suspense>
     </div>
