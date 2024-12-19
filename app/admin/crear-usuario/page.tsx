3ed27e78dcaa6f2535cf4c252b1052bc47c2274c
Pablo <pablocortes1101@gmail.com>
2024-12-19 01:49:53 -0300

    feat: agregar boton para volver a lista de usuarios

--- app/admin/crear-usuario/page.tsx
+++ app/admin/crear-usuario/page.tsx
@@ -1,9 +1,13 @@
+import { BackButton } from "@/components/back-button";
 import CreateUserForm from "@/components/create-user-form";
 
 export default function CreateTicketPage() {
   return (
     <div className="container mx-auto py-10 md:px-10 flex justify-center">
-      <CreateUserForm role="Admin" />
+      <div className="space-y-6">
+        <BackButton href="/admin/usuarios" text="Volver a la lista" />
+        <CreateUserForm role="Admin" />
+      </div>
     </div>
   );
 }
